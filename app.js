469c532e9a51431b01cbd85975faa51e9b80e718
c4rlosc7 <camartinez7@hotmail.com>
2018-03-03 20:34:33 -0500

    init snake

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 ;(function(){
-
+  /* Defined Class Square */
+  class Square {
+    constructor(x, y) {
+      this.x = x
+      this.y = y
+    }
+    draw(){
+      ctx.fillRect(this.x, this.y, 10, 10)
+    }
+  }
+  /* Defined class Snake */
   class Snake {
     constructor() {
+      this.head = new Square(100, 10)
+      this.draw()
     }
-    
+    /* Defined method draw */
+    draw(){
+      this.head.draw()
+    }
   }
 
   const canvas = document.getElementById('canvas')
   const ctx = canvas.getContext('2d')
-  ctx.fillRect(20, 20, 50, 50)
+  //ctx.fillRect(20, 20, 50, 50)
   /*fillRect(posX, posY, width, height)*/
 
   const snake = new Snake()
