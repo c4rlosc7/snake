25469ce350b7601f3c32c892ee40bc5a5b792d76
c4rlosc7 <camartinez7@hotmail.com>
2018-03-03 20:58:01 -0500

    add event listener on global object window

--- app.js
+++ app.js
@@ -41,13 +41,16 @@
   const ctx = canvas.getContext('2d')
   //ctx.fillRect(20, 20, 50, 50)
   /*fillRect(posX, posY, width, height)*/
-
   const snake = new Snake()
-  
+  // Global object window
+  window.addEventListener("keydown", function(event){
+    console.log(event)
+  })
+
   setInterval(function(){
     snake.right() // move right
     ctx.clearRect(0, 0, canvas.width, canvas.height) // clear canvas
     snake.draw() // draw again
-  }, 1000 / 30) // run each 33 mil
+  }, 1000 / 5) //  each 5 frame per secound
 
 })()
