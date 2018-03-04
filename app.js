4a9c4d24eda2889e00bf4171db5043aa2a744c7e
c4rlosc7 <camartinez7@hotmail.com>
2018-03-04 13:56:09 -0500

    add multi square to snake

--- app.js
+++ app.js
@@ -4,9 +4,43 @@
     constructor(x, y) {
       this.x = x
       this.y = y
+      this.back = null
     }
     draw(){
       ctx.fillRect(this.x, this.y, 10, 10)
+      if (this.hasBack()) {
+        this.back.draw()
+      }
+    }
+    add(){
+      if (this.hasBack()) return this.back.add();
+      this.back = new Square(this.x, this.y)
+    }
+    hasBack(){
+      return this.back !== null
+    }
+    copy(){
+      if (this.hasBack()) {
+        this.back.copy()
+        this.back.x = this.x
+        this.back.y = this.y
+      }
+    }
+    right(){
+      this.copy()
+      this.x += 10
+    }
+    left(){
+      this.copy()
+      this.x -= 10
+    }
+    up(){
+      this.copy()
+      this.y -= 10
+    }
+    down(){
+      this.copy()
+      this.y += 10
     }
   }
   /* Defined class Snake */
@@ -15,6 +49,9 @@
       this.head = new Square(100, 10)
       this.draw()
       this.direction = "right"
+      this.head.add()
+      this.head.add()
+      this.head.add()
     }
     /* Defined method draw */
     draw(){
@@ -22,19 +59,25 @@
     }
     /* Defined method draw */
     right(){
-      this.head.x += 10;
+      this.direction = "right"
     }
     /* Defined method left */
     left(){
-      this.head.x -= 10;
+      this.direction = "left"
     }
     /* Defined method up */
     up(){
-      this.head.y -= 10;
+      this.direction = "up"
     }
     /* Defined method down */
     down(){
-      this.head.y += 10;
+      this.direction = "down"
+    }
+    move(){
+      if(this.direction === "up") return this.head.up()
+      if(this.direction === "down") return this.head.down()
+      if(this.direction === "right") return this.head.right()
+      if(this.direction === "left") return this.head.left()
     }
   }
 
@@ -46,14 +89,17 @@
   // Global object window  left=37, up=38, right=39, down=40
   window.addEventListener("keydown", function(event){
     console.log(event.keyCode)
+    event.preventDefault()
     if (event.keyCode === 37) return snake.left();
     if (event.keyCode === 38) return snake.up();
     if (event.keyCode === 39) return snake.right();
     if (event.keyCode === 40) return snake.down();
+    return false
   })
 
   setInterval(function(){
     //snake.right() // move right
+    snake.move()
     ctx.clearRect(0, 0, canvas.width, canvas.height) // clear canvas
     snake.draw() // draw again
   }, 1000 / 5) //  each 5 frame per secound
